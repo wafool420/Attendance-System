2f9baec305d404476bd55324a11cd1fac11befed
wafooI420 <kurtronnel761@gmail.com>
2026-05-25 16:28:42 +0800

    Minor Fix

--- mainProject/mainProject/urls.py
+++ mainProject/mainProject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
 from dashboardApp import views
