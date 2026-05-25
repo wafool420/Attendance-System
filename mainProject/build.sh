2755a15cacc63226b87b59c9e863787086f34a5b
wafooI420 <kurtronnel761@gmail.com>
2026-05-25 15:10:04 +0800

    Minor Fixes

--- mainProject/build.sh
+++ mainProject/build.sh
@@ -3,3 +3,29 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and password:
+    user, created = User.objects.get_or_create(
+        username=username,
+        defaults={'email': email or ''}
+    )
+    user.email = email or user.email
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password(password)
+    user.save()
+
+    print('Superuser created/updated:', username)
+else:
+    print('Superuser environment variables not set.')
+"
