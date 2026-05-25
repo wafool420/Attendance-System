39b7420a03d295b02f0b56f91b416701bc3e87cc
wafooI420 <kurtronnel761@gmail.com>
2026-05-25 16:13:12 +0800

    Configure media files

--- mainProject/mainProject/settings.py
+++ mainProject/mainProject/settings.py
@@ -127,6 +127,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 
 LOGIN_URL = 'login'
